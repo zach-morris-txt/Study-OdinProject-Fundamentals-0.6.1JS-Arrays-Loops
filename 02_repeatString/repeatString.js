4ecc8e7f2f9318977503229f83fc75efa3446443
zach-morris-txt <zach@developer-morris.com>
2023-11-07 16:53:48 -0500

    Array&Loop practice problems 1-3 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringConcat = ""
+    if (num > 0) {
+        for (let i = 0; i < num; i++) {
+            stringConcat += string
+        }
+        return stringConcat
+    } else if (num === 0) { //Handle 0 repeats
+        return ""
+    }
+    else { //Handle negative repeats
+        return "ERROR"
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringReversed = ""
+    for (let i = 0; i < string.length; i++) {
+        stringReversed = stringReversed.concat(string[string.length - (i + 1)])     //fill empty string with each string value reverse order
+    }
+    return stringReversed
 };
 
 // Do not edit below this line
